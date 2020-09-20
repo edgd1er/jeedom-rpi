df6d18f96e5b1e812c080700d70fcf5e0b64c52d
edgd1er <edgd1er@hotmail.com>
2020-09-20 06:06:35 +0200

    change healthcheck

--- gen_image_buildx.sh
+++ gen_image_buildx.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 #Variables
-localDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+localDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 DKRFILE=${localDir}/Docker/Dockerfile.buildx
 ARMDKRFILE=${DKRFILE}.armhf
 ARCHI=$(dpkg --print-architecture)
 IMAGE=jeedom-rpi
-isMultiArch=$(docker buildx ls | grep -c arm )
+isMultiArch=$(docker buildx ls | grep -c arm)
 aptcacher=$(ip route get 1 | awk '{print $7}')
 
 #exit on error
@@ -16,11 +16,11 @@ set -xe
 #use buildx
 
 # add cross build tag if we are on a x86
-generateDockerfileARM(){
-    if [ "${ARCHI}" != "armhf" ]; then
-        sed '/^MAINTAINER .*/i RUN \[ "cross\-build-start" \]' ${DKRFILE} > ${ARMDKRFILE}
-        echo 'RUN [ "cross-build-end" ]' | tee -a ${ARMDKRFILE}
-    fi
+generateDockerfileARM() {
+  if [ "${ARCHI}" != "armhf" ]; then
+    sed '/^MAINTAINER .*/i RUN \[ "cross\-build-start" \]' ${DKRFILE} >${ARMDKRFILE}
+    echo 'RUN [ "cross-build-end" ]' | tee -a ${ARMDKRFILE}
+  fi
 }
 
 #Main
@@ -35,17 +35,18 @@ VERSION="release"
 #V4
 #VERSION="V4-stable"
 
-WHERE="--load"
 WHERE="--push"
+WHERE="--load"
 # x86
 DISTRO="debian"
-[[ $VERSION == "release"  ]] && VERS="v3" || VERS="v4"
+[[ $VERSION == "release" ]] && VERS="v3" || VERS="v4"
 TAG="edgd1er/jeedom-rpi:${VERS}-latest"
 
 aptCacher=""
 
+PTF=linux/arm/v7
 if [ "$(uname -m | cut -c1-3)" != "arm" ]; then
-    docker buildx build ${WHERE} --platform linux/amd64,linux/arm64/v8,linux/arm/v7 -f ${DKRFILE} --build-arg VERSION=$VERSION --build-arg DISTRO=$DISTRO --build-arg aptcacher=$aptcacher -t $TAG ./Docker
-    else
-    docker buildx build ${WHERE} --platform linux/arm/v7 -f ${DKRFILE} --build-arg VERSION=$VERSION --build-arg DISTRO=$DISTRO --build-arg aptcacher=$aptcacher -t $TAG ./Docker
+  PTF=linux/amd64,linux/arm64/v8,linux/arm/v7
 fi
+
+docker buildx build ${WHERE} --platform ${PTF} -f ${DKRFILE} --build-arg VERSION=$VERSION --build-arg DISTRO=$DISTRO --build-arg aptcacher=$aptcacher -t $TAG ./Docker
