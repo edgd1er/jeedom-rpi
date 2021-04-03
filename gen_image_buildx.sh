d37de6f77079eb412f1dcbc984b2cd4dbc192b2b
edgd1er <edgd1er@hotmail.com>
2021-04-03 15:59:49 +0200

    update local building tool

--- gen_image_buildx.sh
+++ gen_image_buildx.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 
+# Local build for images tests
+# multi arch possible for arm/V[6-7], amd64
+
 #Variables
 localDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DKRFILE=${localDir}/Docker/Dockerfile.buildx
 ARCHI=$(dpkg --print-architecture)
 IMAGE=jeedom-rpi
 DUSER=docker_login
-isMultiArch=[[ $("${ARCHI}" != "armhf" ]] && (docker buildx ls | grep -c arm))
+[[ "${ARCHI}" != "armhf" ]] && isMultiArch=$(docker buildx ls | grep -c arm)
 aptCacher=$(ip route get 1 | awk '{print $7}')
 #PROGRESS=plain  #text auto plain
 PROGRESS=auto  #text auto plain
+#cache "--no-cache"
 CACHE=""
+# load => sdocker, push => registry
 WHERE="--load"
 
 #exit on error
 set -xe
+
+#fonctions
+enableMultiArch() {
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker buildx rm amd-arm
+  docker buildx create --use --name amd-arm --driver-opt image=moby/buildkit:master --platform=linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6
+  docker buildx inspect --bootstrap amd-arm
+}
 
 #Main
 [[ "$HOSTNAME" =~ holdom ]] && aptCacher=""
@@ -26,7 +39,8 @@ set -xe
 #VERSION="release"
 #V4
 VERSION="V4-stable"
-#WHERE="--push"
+WHERE="--push"
+WHERE="--load"
 #CACHE="--no-cache"
 
 # x86
@@ -34,15 +48,20 @@ DISTRO="debian"
 [[ $VERSION == "release" ]] && VERS="v3" || VERS="v4"
 TAG="${DUSER}/${IMAGE}:${VERS}-latest"
 
-if [ "${ARCHI}" == "armhf" ]; then
-    PTF=linux/arm/v7
-  else
-    PTF=linux/amd64
-    [[ $isMultiArch -gt 0 ]] && PTF=linux/arm/v7,linux/arm/v6,linux/amd64
+PTF=linux/arm/v7
+#build multi arch images
+if [ "${ARCHI}" == "amd64" ]; then
+  PTF=linux/amd64
+  # load is not compatible with multi arch build
+  if [[ $WHERE == "--push" ]]; then
+    PTF+=,linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm/v6
+    #enable multi arch build framework
+    [[ $isMultiArch -eq 0 ]] && enableMultiArch
+  fi
 fi
 
 # when building multi arch, load is not possible
-[[ $PTF =~ , ]] && WHERE="--push"
+#[[ $PTF =~ , ]] && WHERE="--push"
 
 echo -e "\nbuilding $TAG with version $VERSION on os $DISTRO using cache $CACHE and apt cache $aptCacher \n\n"
 
