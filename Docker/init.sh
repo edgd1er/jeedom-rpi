88d4f1ddecbad48082ef4bf717ca9de90a3b9382
edgd1er <edgd1er@hotmail.com>
2021-04-03 15:59:17 +0200

    fix install.sh bugs, split image building into two stages
    remove man pages

--- Docker/init.sh
+++ Docker/init.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
+set -e
 
 #Variables
 LOGS_TO_STDOUT=${LOGS_TO_STDOUT:-"n"}
 
 ##Functions
+setTimeZone(){
+  [[ ${TZ} == $(cat /etc/timezone) ]] && return
+  echo "Setting timezone to ${TZ}"
+  ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime
+  dpkg-reconfigure -f non-interactive tzdata
+}
 
 mysql_sql() {
   if [ "localhost" == "${MYSQL_JEEDOM_HOST}" ]; then
@@ -44,7 +50,13 @@ step_8_jeedom_configuration() {
 }
 
 ### Main
-echo 'Start init'
+if [[ 1 -eq ${DEBUG:-0} ]]; then
+  set -x
+  SETX="-x"
+  supervisorctl start sshd
+  else
+    SETX=""
+fi
 
 if ! [ -f /.dockerinit ]; then
   touch /.dockerinit
@@ -64,12 +76,19 @@ sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:80/" /etc/apache2/sites-avai
 a2enmod ssl
 sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:${APACHE_HTTPS_PORT}/" /etc/apache2/sites-available/default-ssl.conf
 a2ensite default-ssl
+#fix jeedom install.sh for unattended install
+sed -i "s#^MYSQL_JEEDOM_PASSWD=\$.*#MYSQL_JEEDOM_PASSWD=\$\(tr -cd \'a-f0-9\' \< /dev/urandom \| head -c 15\)#" /root/install_docker.sh
+#apt-get for unattended install
+sed -i "s#apt #apt-get #" /root/install_docker.sh
 
 if [ -f /var/www/html/core/config/common.config.php ]; then
-  echo 'Jeedom is already install'
+  echo 'Jeedom is already installed'
 else
   #generate db param
   WEBSERVER_HOME=/var/www/html
+  #fix jeedom-core, allowing to define mysql not being local
+  sed -i "s#mysql -uroot#mysql -uroot -p${MYSQL_ROOT_PASSWD} -h ${MYSQL_JEEDOM_HOST} -P${MYSQL_JEEDOM_PORT} ${MYSQL_JEEDOM_DBNAME}#" /root/install_docker.sh
+  grep "mysql -u" /root/install_docker.sh
   #S8 =  db param done when running docker.
   while true; do
     result=$(mysql_sql "show grants for 'jeedom'@'%';")
@@ -81,31 +100,45 @@ else
   done
   #mysql is not local to jeedom container
   #step_8_mysql_create_db
+  #remove user save if exists
+  res=$(mysql -BNr -u${MYSQL_JEEDOM_USERNAME} -p${MYSQL_JEEDOM_PASSWD} -h ${MYSQL_JEEDOM_HOST} -P${MYSQL_JEEDOM_PORT} ${MYSQL_JEEDOM_DBNAME} -e "select count(login) from user where login = 'admin';")
+  if [[ 1 -eq $res ]]; then
+    echo "User admin already exists, removing its creation"
+    sed -i 's/\$user->save();//' /var/www/html/install/install.php
+  else
+    echo "User admin does not exists, install will install it."
+  fi
   step_8_jeedom_configuration
   #tempo
   if [[ "release" == "$VERSION" ]]; then
+    #V3
     while true; do
-      mysql -u${MYSQL_JEEDOM_USERNAME} -p${MYSQL_JEEDOM_PASSWD} -h ${MYSQL_JEEDOM_HOST} -P${MYSQL_JEEDOM_PORT} ${MYSQL_JEEDOM_DBNAME} <${WEBSERVER_HOME}/install/install.sql
+      #mysql -u${MYSQL_JEEDOM_USERNAME} -p${MYSQL_JEEDOM_PASSWD} -h ${MYSQL_JEEDOM_HOST} -P${MYSQL_JEEDOM_PORT} ${MYSQL_JEEDOM_DBNAME} <${WEBSERVER_HOME}/install/install.sql
+      echo done
       [[ $? -eq 0 ]] && break
+      echo "Waiting for database to be up"
       sleep 5
     done
     #S9 =  install.php done when running docker.
-    bash -x /root/install_docker.sh -s 9
+    bash ${SETX} /root/install_docker.sh -s 9
     #s10 = post install (cron ) /s11 for v4
-    /root/install_docker.sh -s 10
+    bash ${SETX} /root/install_docker.sh -s 10
     #s11 = jeedom check
-    /root/install_docker.sh -s 11
+    bash ${SETX} /root/install_docker.sh -s 11
   else
+    #V4-stable
+    #S9 drop jeedom database
     #s10 jeedom_installation
-    /root/install_docker.sh -s 10
+    bash ${SETX} /root/install_docker.sh -s 10
     #s10 = post install (cron ) /s11 for v4
-    /root/install_docker.sh -s 11
+    bash ${SETX} /root/install_docker.sh -s 11
     #s12 = jeedom_check
-    /root/install_docker.sh -s 12
+    bash ${SETX} /root/install_docker.sh -s 12
   fi
 fi
 
 echo 'All init complete'
+setTimeZone
 chmod 777 /dev/tty*
 #chmod 777 -R /tmp
 #chmod 755 -R /var/www/html
