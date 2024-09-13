e6d87200da9a70e5208532a070b534b046069b14
edgd1er <3601462+edgd1er@users.noreply.github.com>
2024-09-13 08:19:26 +0200

    do not cache final stage
    as cache may not run install_docker.sh no change is detected in dockerfile.

--- Docker/extras.sh
+++ Docker/extras.sh
@@ -82,9 +82,10 @@ installDep() {
   apt-get update
   apt-get install -y ca-certificates curl gnupg jq
   mkdir -p /etc/apt/keyrings
-  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
-  NODE_MAJOR=18
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  #curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  #NODE_MAJOR=20
+  #echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  ${WEBSERVER_HOME}/resources/install_nodejs.sh
   apt-get install -y --no-install-recommends build-essential g++ python3-dev nodejs
   python3 -m pip install ${BKS} --upgrade pip
   #old style dependencies
