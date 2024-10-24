97f25683c0579ae058c7709db35644b8c2bb3b9d
edgd1er <3601462+edgd1er@users.noreply.github.com>
2024-10-24 23:39:28 +0200

    bump to 4.4.19

--- Docker/extras.sh
+++ Docker/extras.sh
@@ -15,7 +15,7 @@ E_PUSH=${E_PUSH:-0}
 # force zwave-ui as external container + version
 E_ZWAVE=${E_ZWAVE:-0}
 #Default zwavejs-ui version
-E_ZWAVEVER=${E_ZWAVEVER:-"9.21.1"}
+E_ZWAVEVER=${E_ZWAVEVER:-"9.24.0"}
 #Debian 12 needs --break-system-packages
 BKS=""
 
